576326e20175ca07a0bb9b4db270ec941343692a
Ethan Herman <59893598+eherman247@users.noreply.github.com>
2022-05-20 15:39:08 -0600

    Co-authored-by: Joseph Hawkins <joehawkens@users.noreply.github.com>

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import ProductData from './productData.js'
+import ProductList from './productList.js'
+
+const product_data = new ProductData()
+console.log(product_data)
+
+const product_list = new ProductList()
+console.log(product_list)
