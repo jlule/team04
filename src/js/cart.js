25dfd1cfbdad7f68669f9051c9c4647ce508134f
Ethan Herman <59893598+eherman247@users.noreply.github.com>
2022-06-08 11:08:26 -0600

    team 6 working (just barely)

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,47 +1,57 @@
 import { loadHeaderFooter} from './utils.js';
+import CartList from '.cartList.js';
+
 loadHeaderFooter();
 
-function getLocalStorage(key) {
-
-  // Get all items from storage and put into an array and then return array.
-
-  return JSON.parse(localStorage.getItem(key));
-
-
-
+const cart = new CartList('so-cart', document.querySelector('.product-list'));
+cart.init();
+//check to see if there is anything in the cart...
+if (cart.total > 0) {
+  // show our checkout button and total if there are items in the cart.
+  document.querySelector('.list-footer').classList.remove('hide');
 }
 
+// function getLocalStorage(key) {
 
-function getCartContents() {
-  let markup = "";
-  const cartItem = getLocalStorage(localStorage.key(0));
-  const item = [];
-  item.push(cartItem)
-  const htmlItems = item.map((item) => renderCartItem(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
-}
+//   // Get all items from storage and put into an array and then return array.
+
+//   return JSON.parse(localStorage.getItem(key));
 
 
-function renderCartItem(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
 
-  console.log(newItem);
-  return newItem;
-}
+// }
 
 
-getCartContents();
+// function getCartContents() {
+//   let markup = "";
+//   const cartItem = getLocalStorage(localStorage.key(0));
+//   const item = [];
+//   item.push(cartItem)
+//   const htmlItems = item.map((item) => renderCartItem(item));
+//   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+//   // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
+// }
+
+
+// function renderCartItem(item) {
+//   const newItem = `<li class="cart-card divider">
+//   <a href="#" class="cart-card__image">
+//     <img
+//       src="${item.Image}"
+//       alt="${item.Name}"
+//     />
+//   </a>
+//   <a href="#">
+//     <h2 class="card__name">${item.Name}</h2>
+//   </a>
+//   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+//   <p class="cart-card__quantity">qty: 1</p>
+//   <p class="cart-card__price">$${item.FinalPrice}</p>
+// </li>`;
+
+//   console.log(newItem);
+//   return newItem;
+// }
+
+
+// getCartContents();
--- src/js/productData.js
+++ src/js/productData.js
@@ -14,16 +14,29 @@ export default class ProductData  {
     // this.path = `../json/${this.category}.json`;
    
   }
+
   getData(category) {
     // instead we will pass the category we want in here when we need it.
     return fetch(baseURL + `products/search/${category}`)
       .then(convertToJson).then((data) => data.Result);
   }
+
   async findProductById(id) {
     //const products = await this.getData()
     //return products.find((item) => item.Id === id);
     // the API allows us to pull products directly from it by ID...so we can change this method as well to take advantage of that.
     return await fetch(baseURL + `product/${id}`).then(convertToJson)
       .then((data) => data.Result);
+  }
+
+  async checkout(payload) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + 'checkout/', options).then(convertToJson);
   }
 }
